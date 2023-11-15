bdd296910d5b353b663b073a4b315c59c05be151
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2023-11-15 16:48:58 +0500

    More loadout updates

--- CounterFall/Source/Loadout/loadout.gd
+++ CounterFall/Source/Loadout/loadout.gd
@@ -3,17 +3,19 @@ extends ColorRect
 var currently_open: String = "Main"
 
 func _ready():
-	find_child("MeleeButton").pressed.connect(_on_MeleeButton_pressed)
 	for window in get_children():
 		window.hide()
 		if window.has_node("Header/Back"):
 			window.get_node("Header/Back").pressed.connect(_on_Back_pressed)
+		var window_button = find_child(window.get_name() + "Button")
+		if window_button:
+			window_button.pressed.connect(_on_WindowButton_pressed.bind(window.get_name()))
 	find_child(currently_open).show()
 
-func _on_MeleeButton_pressed():
+func _on_WindowButton_pressed(window_name):
 	find_child(currently_open).hide()
-	find_child("Melee").show()
-	currently_open = "Melee"
+	find_child(window_name).show()
+	currently_open = window_name
 
 func _on_Back_pressed():
 	find_child(currently_open).hide()
--- CounterFall/Source/UserInterface/Buttons/GraphicsButton.gd
+++ CounterFall/Source/UserInterface/Buttons/GraphicsButton.gd
@@ -1,10 +1,12 @@
 extends TextureButton
 
 @export var label: String = ""
+@export var font_size: int = 24
 @export var normal_colour: Color = Color(0.25, 0.25, 0.25, 1.0)
 @export var hover_colour: Color = Color(1.0, 1.0, 1.0, 1.0)
 
 func _ready():
+	$Label.set("theme_override_font_sizes/font_size", font_size)
 	$Label.text = label
 	$Label.hide()
 	self_modulate = normal_colour
